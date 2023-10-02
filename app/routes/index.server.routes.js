efeb1c0cc3c98ee13ab477ad8eee54dd8ec3f332
irony05 <ronyhossain110502@gmail.com>
2023-10-02 13:40:41 -0400

    NEW
    time 1:40 PM
    Date: 2023-10-02

---
+++ app/routes/index.server.routes.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req,res,next){
+    res.render('index',{title: 'Home' });
+});
+router.get('/home',function(req,res,next){
+    res.render('home',{title: 'Home'})
+})
+
+router.get('/about',function(req,res,next){
+    res.render('about',{title: 'About' });
+});
+
+
+router.get('/contact',function(req,res,next){
+    res.render('contact',{title: 'Contact' });
+});
+router.get('/services',function(req,res,next){
+    res.render('services',{title: 'Services' });
+});
+
+router.get('/project',function(req,res,next){
+    res.render('project', { title: 'project' });
+
+});
+
+
+module.exports = router;
